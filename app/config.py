c02a8a494fff7267560a70e641f62acf7ab9afc1
MathiasVDS1 <Mvanderstricht@hotmail.com>
2024-11-29 19:14:06 +0100

    New code

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
 class Config: 
     SECRET_KEY = 'Group16Assignment!'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.vvbfnkmgyebcnhqdamxd:Group16Assignment!@aws-0-eu-central-1.pooler.supabase.com:6543/postgres'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.vvbfnkmgyebcnhqdamxd:[YOUR-PASSWORD]@aws-0-eu-central-1.pooler.supabase.com:6543/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
