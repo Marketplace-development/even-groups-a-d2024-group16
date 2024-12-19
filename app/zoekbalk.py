c7068b3a1afee0db6ad30ca5c94baec71bb9149b
Tvenis-19 <thomasvanenis04@gmail.com>
2024-12-19 21:11:16 +0100

    Update zoekbalk.py

--- app/zoekbalk.py
+++ app/zoekbalk.py
@@ -1,10 +1,19 @@
 from app.models import Recipe
 
+import nltk
+from nltk.stem import PorterStemmer
+from app.models import Recipe
 from sqlalchemy import func, cast, String
+
+# Zorg ervoor dat NLTK de benodigde data downloadt
+nltk.download('punkt')
+
+# Initialiseer de stemmer
+stemmer = PorterStemmer()
 
 def apply_search(query, search_term):
     """
-    Past een zoekbalkfilter toe op een query.
+    Past een zoekbalkfilter toe op een query, inclusief het matchen van gestemde woorden.
 
     Parameters:
         query (SQLAlchemy Query): De query waarop het zoekfilter wordt toegepast.
@@ -15,12 +24,22 @@ def apply_search(query, search_term):
     """
     if search_term:
         search_term = search_term.lower().strip()
+
+        # Stem de zoekterm naar de basisvorm
+        stemmed_term = stemmer.stem(search_term)
+
         query = query.filter(
             func.lower(Recipe.recipename).like(f"%{search_term}%") |
+            func.lower(Recipe.recipename).like(f"%{stemmed_term}%") |
             func.lower(cast(Recipe.ingredients, String)).like(f"%{search_term}%") |
+            func.lower(cast(Recipe.ingredients, String)).like(f"%{stemmed_term}%") |
             func.lower(Recipe.description).like(f"%{search_term}%") |
+            func.lower(Recipe.description).like(f"%{stemmed_term}%") |
             func.lower(Recipe.category).like(f"%{search_term}%") |
-            func.lower(Recipe.origin).like(f"%{search_term}%")
+            func.lower(Recipe.category).like(f"%{stemmed_term}%") |
+            func.lower(Recipe.origin).like(f"%{search_term}%") |
+            func.lower(Recipe.origin).like(f"%{stemmed_term}%")
         )
     return query
+
 
